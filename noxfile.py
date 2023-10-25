1ed3e5f41e680282e93c94cd538c8aa5d17ca465
Jimbroze <james.n.dickinson@gmail.com>
2023-10-25 13:11:47 +0100

    Remove Python 3.7 due to security vulnerability
    Tornado is required by Sphinx autobuild. The only versions which are not flagged by Security require at least Python 3.8. 3.7 will be passed EOL soon so I'm happy to remove it.

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 package = "boss_bus"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.10", "3.9", "3.8"]
 nox.needs_version = ">= 2022.1.7"
 nox.options.sessions = (
     "pre-commit",
