0115336935b599fd555446ba707eb423e27b952c
Jimbroze <james.n.dickinson@gmail.com>
2023-11-04 21:01:11 +0000

    Add Event Bus and dispatch method

--- src/boss_bus/message_bus.py
+++ src/boss_bus/message_bus.py
@@ -6,7 +6,13 @@ Classes:
 """
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Sequence
+
 from boss_bus.command_bus import CommandBus, CommandHandler, SpecificCommand
+from boss_bus.event_bus import Event, EventBus
+
+if TYPE_CHECKING:
+    from boss_bus.interface import SupportsHandle
 
 
 class MessageBus:
@@ -22,9 +28,12 @@ class MessageBus:
         Testing...
     """
 
-    def __init__(self, command_bus: CommandBus | None = None) -> None:
+    def __init__(
+        self, command_bus: CommandBus | None = None, event_bus: EventBus | None = None
+    ) -> None:
         """Creates a Message Bus."""
         self.command_bus = command_bus if command_bus is not None else CommandBus()
+        self.event_bus = event_bus if event_bus is not None else EventBus()
 
     def execute(
         self,
@@ -43,3 +52,22 @@ class MessageBus:
             Testing...
         """
         self.command_bus.execute(command, handler)
+
+    def dispatch(
+        self, event: Event, handlers: Sequence[SupportsHandle] | None = None
+    ) -> None:
+        """Dispatch events to their handlers.
+
+        Handlers can be dispatched directly or pre-registered with 'add_handlers'.
+        Previously registered handlers dispatch first.
+
+        Example:
+            >>> from tests.examples import ExampleEvent, ExampleEventHandler
+            >>> bus = MessageBus()
+            >>> test_handler = ExampleEventHandler()
+            >>> test_event = ExampleEvent("Testing...")
+            >>>
+            >>> bus.dispatch(test_event, [test_handler])
+            Testing...
+        """
+        self.event_bus.dispatch(event, handlers)
--- tests/test_message_bus.py
+++ tests/test_message_bus.py
@@ -2,6 +2,7 @@ import pytest
 from typeguard import TypeCheckError
 
 from boss_bus.command_bus import CommandBus
+from boss_bus.event_bus import EventBus
 from boss_bus.message_bus import MessageBus
 from tests.examples import (
     ExampleCommand,
@@ -12,11 +13,13 @@ from tests.examples import (
 
 
 class TestMessageBus:
-    def test_can_created_with_custom_command_bus(self) -> None:
+    def test_can_created_with_custom_command_and_event_bus(self) -> None:
         command_bus = CommandBus()
-        bus = MessageBus(command_bus=command_bus)
+        event_bus = EventBus()
+        bus = MessageBus(command_bus=command_bus, event_bus=event_bus)
 
-        bus.command_bus = command_bus
+        assert bus.command_bus == command_bus
+        assert bus.event_bus == event_bus
 
     def test_execute_executes_a_command(self) -> None:
         command = ExampleCommand("Test the bus")
@@ -29,5 +32,21 @@ class TestMessageBus:
         event = ExampleEvent("Test the bus")
         handler = ExampleEventHandler()
         bus = MessageBus()
+
         with pytest.raises(TypeCheckError):
             bus.execute(event, handler)  # type: ignore[type-var, arg-type]
+
+    def test_an_event_can_be_dispatched(self) -> None:
+        event = ExampleEvent("Test the bus")
+        handler = ExampleEventHandler()
+        bus = MessageBus()
+
+        bus.dispatch(event, [handler])
+
+    def test_a_command_cannot_be_dispatched(self) -> None:
+        command = ExampleCommand("Test the bus")
+        handler = ExampleCommandHandler()
+        bus = MessageBus()
+
+        with pytest.raises(TypeCheckError):
+            bus.dispatch(command, handler)  # type: ignore[arg-type]
